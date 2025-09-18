ea03f3ab288f5ded87303508afc2150b91db53f0
_Gagan <84296830+seika-afk@users.noreply.github.com>
2025-09-18 23:05:20 +0530

    Update Retriever.py

--- src/Retriever.py
+++ src/Retriever.py
@@ -1,16 +1,13 @@
 from langchain.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import yaml
-import os
+import yaml, os
 from dotenv import load_dotenv
 
-# Load .env variables
 load_dotenv()
 
 with open("params.yaml", 'r') as f:
     config = yaml.safe_load(f)
 
-# Pass the API key explicitly from .env
 embeddings = GoogleGenerativeAIEmbeddings(
     model="models/embedding-001",
     google_api_key=os.getenv("GEMINI_API_KEY")
@@ -26,4 +23,3 @@ retriever = Vector_Stores.as_retriever(
     search_type="similarity",
     search_kwargs={"k": config["retrieving"]["num_of_retrieved_docs"]}
 )
-
