0346212d775b56de36ddcfb92c08b32a724a1fea
_Gagan <84296830+seika-afk@users.noreply.github.com>
2025-09-18 23:37:41 +0530

    Update Retriever.py

--- src/Retriever.py
+++ src/Retriever.py
@@ -1,21 +1,12 @@
-from langchain.vectorstores import FAISS
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import yaml, os
-from dotenv import load_dotenv
+from langchain_community.vectorstores import FAISS
+import yaml
 
-load_dotenv()
-
-with open("params.yaml", 'r') as f:
+with open("params.yaml", "r") as f:
     config = yaml.safe_load(f)
-
-embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/embedding-001",
-    google_api_key=os.getenv("GEMINI_API_KEY")
-)
 
 Vector_Stores = FAISS.load_local(
     "faiss_index",
-    embeddings,
+    embeddings=None,  
     allow_dangerous_deserialization=True
 )
 
