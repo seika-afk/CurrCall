9c5f2e1249ed1cc74bbfca0ad0d5d147f7a2f309
_Gagan <84296830+seika-afk@users.noreply.github.com>
2025-09-21 22:19:40 +0530

    Update Retriever.py

--- src/Retriever.py
+++ src/Retriever.py
@@ -1,21 +1,16 @@
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
-import yaml
+from langchain_huggingface import HuggingFaceEmbeddings
+import yaml, os
 
 with open("params.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
-
-Vector_Stores = FAISS.load_local(
-    "faiss_index",
-    embeddings,
-    allow_dangerous_deserialization=True
-)
-
-retriever = Vector_Stores.as_retriever(
-    search_type="similarity",
-    search_kwargs={"k": config["retrieving"]["num_of_retrieved_docs"]}
-)
+def get_retriever():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    base_dir = os.path.dirname(__file__)
+    faiss_path = os.path.join(base_dir, "faiss_index")
+    vector_store = FAISS.load_local(faiss_path, embeddings, allow_dangerous_deserialization=True)
+    return vector_store.as_retriever(
+        search_type="similarity",
+        search_kwargs={"k": config.get("retrieving", {}).get("num_of_retrieved_docs", 5)}
+    )
